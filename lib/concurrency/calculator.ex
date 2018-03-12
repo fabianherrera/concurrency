d1caa5141facb4987fd0a96b2610af6dc6837d84
fabianherrera@gmail.com <admnts9000!>
2018-03-12 16:08:49 -0400

    feat: new modules and directories

---
+++ lib/concurrency/calculator.ex
@@ -0,0 +1,20 @@
+defmodule Concurrency.Calculator do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  # use Application
+@doc """
+Substraction function for CurrencyCalculator, it subs a number to a initial value, handled by ConcurrencyCalculator.loop
+"""
+  def sub(_type, _args) do
+
+  end
+  @doc """
+  Add function for CurrencyCalculator, it adds a number to a initial value, handled by ConcurrencyCalculator.loop
+  """
+  def add(_type, _args) do
+
+  end
+
+end
